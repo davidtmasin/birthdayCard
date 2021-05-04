4ecd7a68e4882d82127e48261fbd0dbf0b767fc1
DAVID TEIXEIRA DE MASIN <t_davidt@hapvida.com.br>
2021-05-04 19:36:52 -0300

    Atualizando index e add scripts

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,2 +1,15 @@
 const somParabens = new Audio();
 somParabens.src = 'https://www.myinstants.com/media/sounds/parabens-da-xuxa.mp3';
+
+// Função para poder realizar o download da página HTML como IMG
+function downloadimage(){
+    var container = document.getElementById("htmltoimage");
+    html2canvas(container,{allowTaint : true}).then(function(canvas) {
+    var link = document.createElement("a");
+    document.body.appendChild(link);
+    link.download = "birthdayCard.jpg";
+    link.href = canvas.toDataURL();
+    link.target = "_blank";
+    link.click();
+    });
+   }
