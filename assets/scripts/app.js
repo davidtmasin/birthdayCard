7b523896d51b330d09ba1567041fefdc8d025587
DAVID TEIXEIRA DE MASIN <t_davidt@hapvida.com.br>
2021-05-04 22:10:05 -0300

    Add nome do aniversariante

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,15 +1,26 @@
 const somParabens = new Audio();
 somParabens.src = 'https://www.myinstants.com/media/sounds/parabens-da-xuxa.mp3';
 
-// Função para poder realizar o download da página HTML como IMG
-function downloadimage(){
+
+let nomeAniversariante = prompt('De quem é o aniversário?')
+
+let span = document.getElementById('aniversariante')
+
+span.innerText = nomeAniversariante
+
+
+
+
+//Função para poder realizar o download da página HTML como IMG
+
+function downloadimage() {
     var container = document.getElementById("htmltoimage");
-    html2canvas(container,{allowTaint : true}).then(function(canvas) {
-    var link = document.createElement("a");
-    document.body.appendChild(link);
-    link.download = "birthdayCard.jpg";
-    link.href = canvas.toDataURL();
-    link.target = "_blank";
-    link.click();
+    html2canvas(container, { allowTaint: true }).then(function (canvas) {
+        var link = document.createElement("a");
+        document.body.appendChild(link);
+        link.download = "birthdayCard.jpg";
+        link.href = canvas.toDataURL();
+        link.target = "_blank";
+        link.click();
     });
-   }
+}
